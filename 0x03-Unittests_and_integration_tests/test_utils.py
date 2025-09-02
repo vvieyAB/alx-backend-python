fd2f24baeafb3118f706618633142e5373774286
VvieyAB <ndicuvictoria@gmail.com>
2025-09-02 11:16:29 +0300

    done1

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,15 @@
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test class for access_nested_map function"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test that access_nested_map returns the expected result"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
